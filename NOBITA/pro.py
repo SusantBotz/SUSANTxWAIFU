bf7c7d569434b5d41bde503528ab3e49cd4a19fc
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-06-04 12:00:42 +0530

    Update pro.py

--- NOBITA/pro.py
+++ NOBITA/pro.py
@@ -1,1 +1,9 @@
+from pyrogram import filters
+from pyrogram.types import Message
 
+SUDO_USERS = (5536473064, 8019277081)
+
+async def is_sudo(_, __, message: Message):
+    return message.from_user and message.from_user.id in SUDO_USERS
+
+sudo_filter = filters.create(is_sudo)
